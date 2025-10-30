af23aa6432b3cf8730b4c6eabaf1229b59e04dfa
Flawkee <70835293+Flawkee@users.noreply.github.com>
2025-10-30 08:45:05 +0200

    Updated guid & added assemblyinfo.cs

--- assemblyinfo.cs
+++ assemblyinfo.cs
@@ -1,2 +1,14 @@
-﻿using System.Runtime.InteropServices;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Flawkee")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1.0+d19e6edade31336dc13396e7c1975fce302bbcda")]
+[assembly: System.Reflection.AssemblyProductAttribute("Emby.Ktuvit.Plugin")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Emby.Ktuvit.Plugin")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 [assembly: Guid("634A51FB-8EDE-43AF-8D19-EA9379182D44")]
+
+
